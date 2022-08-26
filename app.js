2ef70cbed128350817913d6422490b72e056eaa4
Michael Kancov <michael.kancov00@gmail.com>
2022-08-25 17:51:20 -0700

    my project files

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var operationHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var allCookieShops = [];
+//get cookie sales into table
